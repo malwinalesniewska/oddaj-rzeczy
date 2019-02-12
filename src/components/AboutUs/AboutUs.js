35af27b842b11bfa325ee86597b250bf87dddae3
Malwina Leśniewska <malwina.lesniewska@gmail.com>
2019-02-12 16:33:06 +0100

    finished about-us section

---
+++ src/components/AboutUs/AboutUs.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+import './_AboutUs.scss';
+import Decoration from "../Decoration/Decoration";
+
+class AboutUs extends Component {
+    render() {
+        return (
+            <section className='about_us'>
+                <div className='about_us__left'>
+                    <h2 className='about_us__title'>O nas</h2>
+                    <Decoration/>
+                    <p className='about_us__text'>
+                        Gumbo beet greens corn soko endive gumbo gourd. Parsley shallot courgette tatsoi pea sprouts fava
+                        bean collard greens dandelion okra wakame tomato. Dandelion cucumber earthnut.
+                    </p>
+                    <div className='about_us__sign'/>
+                </div>
+                <div className='about_us__right'/>
+            </section>
+        );
+    }
+}
+
+export default AboutUs;
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,15 +4,17 @@ import Header from '../Header/Header';
 import LogoSection from '../LogoSection/LogoSection';
 import StatsSection from '../StatsSection/StatsSection';
 import FourSteps from '../FourSteps/FourSteps';
+import AboutUs from "../AboutUs/AboutUs";
 
 class App extends Component {
   render() {
     return (
         <Fragment>
-          <Header/>
-          <LogoSection/>
-          <StatsSection/>
-          <FourSteps/>
+            <Header/>
+            <LogoSection/>
+            <StatsSection/>
+            <FourSteps/>
+            <AboutUs/>
         </Fragment>
     );
   }
