e308c28319662c2619aea3d4bb6ccca52815e6b0
Malwina Leśniewska <malwina.lesniewska@gmail.com>
2019-03-05 21:55:00 +0100

    Updates

---
+++ precache-manifest.5c64f353572d41fd36db7c8a4a8522db.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "1b54a9fdacdebba01f54220098ad47f5",
+    "url": "/oddaj-rzeczy/static/media/header_start_img_edited.1b54a9fd.jpg"
+  },
+  {
+    "revision": "a35a2736ab2ed601305e6ea3cda4b21f",
+    "url": "/oddaj-rzeczy/static/media/footer_bg_img.a35a2736.jpg"
+  },
+  {
+    "revision": "0e48fc7f4f6fe85f5d1a34bdb30a84af",
+    "url": "/oddaj-rzeczy/static/media/about.0e48fc7f.jpg"
+  },
+  {
+    "revision": "36e12db705c80afb26fb",
+    "url": "/oddaj-rzeczy/static/js/runtime~main.36e12db7.js"
+  },
+  {
+    "revision": "1772e343c0206b326ad4",
+    "url": "/oddaj-rzeczy/static/js/main.1772e343.chunk.js"
+  },
+  {
+    "revision": "fbdd2d5b0be08d551362",
+    "url": "/oddaj-rzeczy/static/js/1.fbdd2d5b.chunk.js"
+  },
+  {
+    "revision": "1772e343c0206b326ad4",
+    "url": "/oddaj-rzeczy/static/css/main.1bd22a56.chunk.css"
+  },
+  {
+    "revision": "3a04fb708009ff73869205e01f330ea6",
+    "url": "/oddaj-rzeczy/index.html"
+  }
+];
