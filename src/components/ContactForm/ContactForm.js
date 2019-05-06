44b0efa72618e9976100a7b2ae834d645cf9155c
Malwina Leśniewska <malwina.lesniewska@gmail.com>
2019-05-06 22:38:41 +0200

    added new form validation

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -1,45 +1,89 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
 import './_ContactForm.scss';
 import Decoration from '../Decoration/Decoration';
+import FormValidator from '../FormValidator/FormValidator';
 
 class ContactForm extends Component {
-    state = {
-        name: '',
-        email: '',
-        message: ''
-    };
+    constructor() {
+        super();
+
+        this.validator = new FormValidator([
+            {
+                field: 'email',
+                method: 'isEmpty',
+                validWhen: false,
+                message: 'E-mail jest wymagany'
+            },
+            {
+                field: 'email',
+                method: 'isEmail',
+                validWhen: true,
+                message: 'Wprowadź poprawny adres e-mail'
+            },
+            {
+                field: 'name',
+                method: 'isEmpty',
+                validWhen: false,
+                message: 'Wprowadź imię'
+            },
+            {
+                field: 'name',
+                method: 'matches',
+                args: [/^[a-zA-ZĄąĆćĘęŁłŃńÓóŻżŹź '.-]*$/], // args is an optional array of arguments that will be passed to the validation method
+                validWhen: true,
+                message: 'Wprowadź poprawne imię'
+            },
+            {
+                field: 'message',
+                method: 'isEmpty',
+                validWhen: false,
+                message: 'Wiadomość jest wymagana'
+            }
+        ]);
+
+        this.state = {
+            name: '',
+            email: '',
+            message: '',
+            validation: this.validator.valid()
+        };
+
+        this.submitted = false;
+    }
 
     allInputsHandler = (event) => {
+        event.preventDefault();
+
         this.setState({
             [event.target.name]: event.target.value
         })
     };
 
-    handleSubmit = (event) => {
-        const namePattern = /^[a-zA-Z-,.ąęćźżńłśóĆŻŹŃŁŚÓ']+$/;
-        const mailPattern = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
-        const messagePattern = /^.{1,250}/;
-        let info = [];
+    handleSubmit = event => {
         event.preventDefault();
 
-        if (!namePattern.test(this.state.name)) {
-            info.push('Pole imię musi zostać uzupełnione');
-        }
-        if (!mailPattern.test(this.state.email)) {
-            info.push('Pole email musi zostać uzupełnione i zawierać znak @');
-        }
-        if (!messagePattern.test(this.state.message)) {
-            info.push('Pole wiadomość musi zostać uzupełnione');
-        }
-        if (namePattern.test(this.state.name) && mailPattern.test(this.state.email) && messagePattern.test(this.state.message)) {
-            info = 'Dziękujemy za wysłanie formularza!'
-        }
+        const validation = this.validator.validate(this.state);
+        this.setState({ validation });
+        this.submitted = true;
 
-        alert(info);
+        if (validation.isValid) {
+            // handle actual form submission here
+        }
     };
 
     render() {
+        let validation = this.submitted ?            // if the form has been submitted at least once
+            this.validator.validate(this.state) :   // then check validity every time we render
+            this.state.validation;                 // otherwise just use what's in state
+
         return (
+            <Fragment>
+            <link
+                rel="stylesheet"
+                href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+                integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+                crossOrigin="anonymous"
+            />
             <div className='contact_section'>
                 <div className='contact_section__main'>
                     <p className='contact_section__title'>
@@ -50,30 +94,38 @@ class ContactForm extends Component {
                         <p className='contact_section__form_title'>
                             FORMULARZ KONTAKTOWY
                         </p>
+
                         <div className='contact_section__name_email_inputs'>
-                            <input
-                                className='contact_section__input contact_section__input--name'
-                                type='text'
-                                name='name'
-                                placeholder='Imię'
-                                onChange={this.allInputsHandler}
-                            />
-                            <input
-                                className='contact_section__input contact_section__input--email'
-                                type='email'
-                                name='email'
-                                placeholder='Email'
-                                onChange={this.allInputsHandler}
-                            />
+                            <div className={validation.name.isInvalid ? 'has-error' : undefined}>
+                                <input className='contact_section__input contact_section__input--name'
+                                       type='text'
+                                       name='name'
+                                       placeholder='Imię'
+                                       onChange={this.allInputsHandler}
+                                />
+                                <span className='help-block ml-4'>{validation.name.message}</span>
+                            </div>
+                            <div className={validation.email.isInvalid ? 'has-error' : undefined}>
+                                <input type='email'
+                                       className='contact_section__input contact_section__input--email'
+                                       name='email'
+                                       placeholder='john@doe.com'
+                                       onChange={this.allInputsHandler}
+                                />
+                                <span className='help-block ml-4'>{validation.email.message}</span>
+                            </div>
                         </div>
                         <br/>
-                        <input
-                            className='contact_section__input contact_section__input--message'
-                            type='text'
-                            name='message'
-                            placeholder='Wiadomość'
-                            onChange={this.allInputsHandler}
-                        />
+                        <div className={validation.message.isInvalid ? 'has-error' : undefined} style={{width: '100%'}}>
+                            <input
+                                className='contact_section__input contact_section__input--message'
+                                   type='text'
+                                   name='message'
+                                   placeholder='Wiadomość'
+                                   onChange={this.allInputsHandler}
+                            />
+                            <span className='help-block ml-4'>{validation.message.message}</span>
+                        </div>
                         <br/>
                         <input
                             className='contact_section__input contact_section__input--send'
@@ -97,6 +149,7 @@ class ContactForm extends Component {
                     </div>
                 </footer>
             </div>
+            </Fragment>
         );
     }
 }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App/App'
 import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
